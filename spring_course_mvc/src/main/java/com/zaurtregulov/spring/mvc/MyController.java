442888cfb9e0847545c65dc6a37192b8170d15d4
avv <volgamygit@rambler.ru>
2024-07-06 15:05:47 +0300

    Аннотация @RequestParam

--- spring_course_mvc/src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ spring_course_mvc/src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 // контроллер показывает - какая веб-страница должна отобразиться
 
 @Controller
@@ -23,14 +24,25 @@ public class MyController {
     public String showEmpDetails() {
         return "show-emp-details-view";
     }*/
-    @RequestMapping("/showDetails")
+ /*  @RequestMapping("/showDetails")
     //суть этого метода - получаем имя (с помощью HttpServletRequest) и добавляем в контейнер (модель)
     public String showEmpDetails(HttpServletRequest request, Model model) {
         String empName = request.getParameter("employeeName");
 // модель - это контейнер для каких-угодно данных
 //добавляем empName модель (контейнер для хрнения данных)
         model.addAttribute("nameAttribute", empName);
+        return "show-emp-details-view";
+    }*/
+    @RequestMapping("/showDetails")
+    //суть этого метода - получаем имя (с помощью HttpServletRequest) и добавляем в контейнер (модель)
 
+    //аннотация @RequestParam позволяет связывать поле формы с параметром метода из Controller-а
+    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model) {
+
+// модель - это контейнер для каких-угодно данных
+//добавляем empName модель (контейнер для хрнения данных)
+        model.addAttribute("nameAttribute", empName);
         return "show-emp-details-view";
     }
+
 }

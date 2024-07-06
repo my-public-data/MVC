b064ade2b726897d6e770ebfa2ebeb12c438ff39
avv <volgamygit@rambler.ru>
2024-07-06 14:44:30 +0300

    Компонент Модель

--- spring_course_mvc/src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ spring_course_mvc/src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -1,6 +1,8 @@
 package com.zaurtregulov.spring.mvc;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 // контроллер показывает - какая веб-страница должна отобразиться
 
@@ -17,8 +19,18 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
-    @RequestMapping("/showDetails")
+    /* @RequestMapping("/showDetails")
     public String showEmpDetails() {
+        return "show-emp-details-view";
+    }*/
+    @RequestMapping("/showDetails")
+    //суть этого метода - получаем имя (с помощью HttpServletRequest) и добавляем в контейнер (модель)
+    public String showEmpDetails(HttpServletRequest request, Model model) {
+        String empName = request.getParameter("employeeName");
+// модель - это контейнер для каких-угодно данных
+//добавляем empName модель (контейнер для хрнения данных)
+        model.addAttribute("nameAttribute", empName);
+
         return "show-emp-details-view";
     }
 }
--- spring_course_mvc/src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
+++ spring_course_mvc/src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
@@ -3,7 +3,9 @@
 <html>
     <body>
         <h2>Dear Employee, you are WELCOME!!!</h2>
-        Your name: ${param.employeeName}
+        <%-- Your name: ${param.employeeName}--%>
+
+        Your name: ${nameAttribute}
     </body>
 
 
